c40e5b7eb5ae0155cee6be697046db9f774a0d0a
isharamadawa <isharazone@gmail.com>
2020-08-04 18:33:53 +0530

    Config

--- ChatAscentic/server.js
+++ ChatAscentic/server.js
@@ -1,14 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/chat-ascentic'));
+app.use(express.static('./dist/chat-ascentic'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/chat-ascentic/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/chat-ascentic'}
+  );
 });
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
