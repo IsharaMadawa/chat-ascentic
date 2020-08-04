c8dac04c6b37d74290c1932668e5840301f6f975
isharamadawa <isharazone@gmail.com>
2020-08-04 16:04:06 +0530

    Bug fixing

--- ChatAscentic/src/environments/environment.prod.ts
+++ ChatAscentic/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/',
-  socketUrl: 'http://localhost:4000/'
+  apiUrl: 'http://localhost:4111/',
+  socketUrl: 'http://localhost:1111/'
 };
